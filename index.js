8dabffdb0199c6d40c89951c5a7b401d92a4e8bd
geoffrey <geoffrey.waiganjo@student.moringaschool.com>
2024-07-01 14:11:34 +0300

    lab.md

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    console.log(customerName.toUpperCase())
+}
+upperCaseCustomerName(); 
+
+
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+   
+}
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 1
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 3
+}
